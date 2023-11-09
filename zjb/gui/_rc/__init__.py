7ea3c2fb17af2390d569d9010b241eeaab5b6cfb
wangchen <wangchen@zhejianglab.com>
2023-11-09 16:05:51 +0800

    fix: 修复windows上跨磁盘启动时找不到资源的问题

--- zjb/gui/_rc/__init__.py
+++ zjb/gui/_rc/__init__.py
@@ -1,11 +1,6 @@
-
-
 import os
 
 
-def find_resource_file(name, rel=True):
-    fname = os.path.join(__file__, '../', name)
-    if rel:
-        return os.path.relpath(fname)
-    else:
-        return os.path.abspath(fname)
+def find_resource_file(name):
+    fname = os.path.join(__file__, "../", name)
+    return os.path.abspath(fname)

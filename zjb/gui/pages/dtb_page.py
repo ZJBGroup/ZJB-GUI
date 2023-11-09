2b88311ecb29c05af5169df28c322fa80693c9f8
wangchen <wangchen@zhejianglab.com>
2023-11-09 15:55:51 +0800

    multi: DTB页面相关优化
    fix: B仿真/PSE结果现在会及时更新
    feat: 仿真现在可以配置name和覆盖默认值的参数
    feat: 仿真和PSE现在会生成默认name

--- zjb/gui/pages/dtb_page.py
+++ zjb/gui/pages/dtb_page.py
@@ -1,29 +1,32 @@
+import re
 from functools import partial
+from typing import Any
 
 import numpy as np
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtWidgets import QFormLayout, QVBoxLayout, QWidget
 from qfluentwidgets import (
     BodyLabel,
     FluentIcon,
     LineEdit,
     MessageBoxBase,
+    PrimaryPushButton,
     SmoothScrollArea,
     SubtitleLabel,
     TitleLabel,
     TransparentPushButton,
 )
 
-from zjb.doj.job import GeneratorJob, Job
+from zjb.doj.job import GeneratorJob, Job, JobState
 from zjb.main.api import DTB, PSEResult, SimulationResult
 from zjb.main.dtb.utils import expression2unicode
 
 from .._global import GLOBAL_SIGNAL, get_workspace
-from ..common.utils import show_success
+from ..common.utils import show_error, show_success
 from ..panels.data_operation_panel import DataOperationPanel
 from ..widgets.choose_data_dialog import ChooseDataDialog
 from ..widgets.dict_editor import KeySetDictEditor
-from ..widgets.editor import LineEditor
+from ..widgets.editor import FloatEditor, LineEditor
 from .base_page import BasePage
 from .connectivity_page import ConnectivityPage
 from .dtb_model_page import DTBModelPage
@@ -36,6 +39,14 @@ class DTBPage(BasePage):
         self.dtb = dtb
 
         self._setup_ui()
+
+        self._sync_data()
+        self.currentPageSignal.connect(self._sync_data)
+
+        self._running_jobs: dict[str, Job] = {}
+        self._timer = QTimer()
+        self._timer.timeout.connect(self._poll)
+        self._timer.start(1000)
 
     def _setup_ui(self):
         self.vBoxLayout = QVBoxLayout(self)
@@ -60,11 +71,11 @@ class DTBPage(BasePage):
 
         self.formLayout.addRow(BodyLabel("connectivity:"), btn_connectivity)
 
-        btn_simulate = TransparentPushButton(f"Simulate")
+        btn_simulate = PrimaryPushButton(f"Simulate")
         btn_simulate.clicked.connect(self._simulate)
         self.vBoxLayout.addWidget(btn_simulate)
 
-        btn_pse = TransparentPushButton(f"PSE")
+        btn_pse = PrimaryPushButton(f"PSE")
         btn_pse.clicked.connect(self._pse)
         self.vBoxLayout.addWidget(btn_pse)
 
@@ -76,6 +87,10 @@ class DTBPage(BasePage):
         self.scrollLayout = QFormLayout(self.scrollWidget)
         self.scrollLayout.setLabelAlignment(Qt.AlignmentFlag.AlignRight)
 
+    def _sync_data(self):
+        for i in reversed(range(self.scrollLayout.rowCount())):
+            self.scrollLayout.removeRow(i)
+
         def _create_data_button(name, data):
             btn_data = TransparentPushButton(f"{name}")
             btn_data.clicked.connect(lambda: self._show_data_dialog(name, data))
@@ -84,30 +99,67 @@ class DTBPage(BasePage):
         for name, data in self.dtb.data.items():
             if isinstance(data, SimulationResult) or isinstance(data, PSEResult):
                 _create_data_button(name, data)
+            elif data is None:
+                self.scrollLayout.addRow(TransparentPushButton(f"{name}(running)"))
             else:
                 data_manipulation_panel = DataOperationPanel(name, data)
                 self.scrollLayout.addRow(data_manipulation_panel)
 
+    def _poll(self):
+        _removing = []
+        for name, job in self._running_jobs.items():
+            if job.state == JobState.ERROR:
+                show_error(f"An error occurred while running {name}.", self.window())
+                _removing.append(name)
+                continue
+            if job.state == JobState.DONE:
+                show_success(f"Successfully run {name}.", self.window())
+                _removing.append(name)
+                continue
+        if _removing:
+            self._sync_data()
+        for name in _removing:
+            self._running_jobs.pop(name)
+
     def _simulate(self):
-        job = Job(DTB.simulate, self.dtb)
-        job.kwargs = {"store_key": job._gid.str}
         ws = get_workspace()
         assert ws
+        dialog = SimulationDialog(self.dtb, self)
+        if not dialog.exec():
+            return
+        store_key = dialog.name
+        with self.dtb:
+            data = self.dtb.data
+            if not store_key:
+                store_key = _generate_key(data, "simulation")
+            self.dtb.data = data | {store_key: None}
+        self.scrollLayout.addRow(TransparentPushButton(f"{store_key}(running)"))
+        job = Job(DTB.simulate, self.dtb, store_key=store_key)
+        if dialog.params:
+            job.kwargs |= {"dynamic_parameters": dialog.params}
         ws.manager.bind(job)
+        self._running_jobs[store_key] = job
         GLOBAL_SIGNAL.joblistChanged.emit()
-        show_success(f"Simulation {job._gid.str} started!", self.window())
+        show_success(f"Simulation {store_key} started!", self.window())
 
     def _pse(self):
+        ws = get_workspace()
+        assert ws
         dialog = PSEDialog(self.dtb, self)
         if not dialog.exec():
             return
-        job = GeneratorJob(DTB.pse, self.dtb, dialog.params, store_key=dialog.name)
-        job.kwargs = {"store_key": dialog.name or ("PSE_" + job._gid.str)}
-        ws = get_workspace()
-        assert ws
+        store_key = dialog.name
+        with self.dtb:
+            data = self.dtb.data
+            if not store_key:
+                store_key = _generate_key(data, "pse")
+            self.dtb.data = data | {store_key: None}
+        self.scrollLayout.addRow(TransparentPushButton(f"{store_key}(running)"))
+        job = GeneratorJob(DTB.pse, self.dtb, dialog.params, store_key=store_key)
         ws.manager.bind(job)
+        self._running_jobs[store_key] = job
         GLOBAL_SIGNAL.joblistChanged.emit()
-        show_success(f"PSE {job._gid.str} started!", self.window())
+        show_success(f"PSE {store_key} started!", self.window())
 
     def _click_subject(self):
         subject = self.dtb.subject
@@ -133,6 +185,54 @@ class DTBPage(BasePage):
         w.exec()
 
 
+class SimulationDialog(MessageBoxBase):
+    def __init__(self, dtb: DTB, parent=None):
+        super().__init__(parent)
+
+        self._dtb = dtb
+        self._parameters = dtb.model.dynamics.parameters | dtb.model.parameters
+        self._params = {}
+
+        self._setup_ui()
+
+        self.name_edit.setText(_generate_key(dtb.data, "simulation"))
+
+    def _setup_ui(self):
+        self.viewLayout.addWidget(SubtitleLabel(self.tr("Configure Simulation:")))
+
+        self.formLayout = QFormLayout()
+        self.formLayout.setLabelAlignment(Qt.AlignmentFlag.AlignRight)
+        self.viewLayout.addLayout(self.formLayout)
+
+        self.name_edit = LineEdit()
+        self.name_edit.setPlaceholderText("Input a name here")
+        self.formLayout.addRow(BodyLabel("name:"), self.name_edit)
+
+        self.scrollLayout = SmoothScrollArea()
+        self.scrollLayout.setWidgetResizable(True)
+        self.scrollLayout.setMinimumSize(300, 200)
+        self.formLayout.addRow(BodyLabel("parameters:"), self.scrollLayout)
+
+        self.params_editors = KeySetDictEditor(
+            self._parameters,
+            lambda key: self._parameters[key],
+            FloatEditor,
+            self._params,
+            key2str=partial(expression2unicode, rich=False),
+            listen_items=True,
+            dialog_parent=self.parent(),
+        )
+        self.scrollLayout.setWidget(self.params_editors)
+
+    @property
+    def params(self):
+        return self._params
+
+    @property
+    def name(self):
+        return self.name_edit.text()
+
+
 class PSEDialog(MessageBoxBase):
     def __init__(self, dtb: DTB, parent=None):
         super().__init__(parent)
@@ -143,18 +243,23 @@ class PSEDialog(MessageBoxBase):
 
         self._setup_ui()
 
+        self.name_edit.setText(_generate_key(dtb.data, "pse"))
+
     def _setup_ui(self):
         self.viewLayout.addWidget(SubtitleLabel(self.tr("Configure PSE:")))
 
+        self.formLayout = QFormLayout()
+        self.formLayout.setLabelAlignment(Qt.AlignmentFlag.AlignRight)
+        self.viewLayout.addLayout(self.formLayout)
+
         self.name_edit = LineEdit()
         self.name_edit.setPlaceholderText("Input a name here")
-
-        self.viewLayout.addWidget(self.name_edit)
+        self.formLayout.addRow(BodyLabel("name:"), self.name_edit)
 
         self.scrollLayout = SmoothScrollArea()
         self.scrollLayout.setWidgetResizable(True)
-        self.scrollLayout.setMinimumSize(300, 200)
-        self.viewLayout.addWidget(self.scrollLayout)
+        self.scrollLayout.setMinimumSize(280, 180)
+        self.formLayout.addRow(BodyLabel("parameters:"), self.scrollLayout)
 
         self.params_editors = KeySetDictEditor(
             self._parameters,
@@ -185,3 +290,15 @@ class PSEDialog(MessageBoxBase):
     def _ps_changed(self, _):
         sizes = [str(len(para)) for para in self._params.values()]
         self.stat_label.setText(f"{'×'.join(sizes)} parameters will be simulated")
+
+
+def _generate_key(_dict: dict[str, Any], prefix: str):
+    pattern = re.compile(rf"{prefix}-(\d+)")
+    _max = 0
+    for name in _dict:
+        if res := pattern.fullmatch(name):
+            value = int(res.groups()[0])
+            if value > _max:
+                _max = value
+    _max += 1
+    return f"{prefix}-{_max}"

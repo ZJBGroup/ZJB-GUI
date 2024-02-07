a77031b8c6008e33cfb7a1072fe9e9bd6638cdf9
wangchen <wangchen@zhejianglab.com>
2024-02-07 17:12:36 +0800

    build: 更新版本到0.2.6

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ import setuptools
 
 setuptools.setup(
     name="zjb-gui",
-    version="0.2.5",
+    version="0.2.6",
     description="The GUI of Zhejiang Lab's digital twin brain platform",
     author="ZJB Group",
     url="https://github.com/ZJBGroup/ZJB-GUI",
     license="GPL-3.0",
     install_requires=[
-        "zjb-main @ https://github.com/ZJBGroup/ZJB-Main/tarball/main",
+        "zjb-main @ https://github.com/ZJBGroup/ZJB-Main/archive/refs/tags/v0.2.6.tar.gz",
         "PyQt-Fluent-Widgets",
         "lmdb",
         "qtconsole",
         "requests",
         "QCustomPlot_PyQt5",
-        "pyqtgraph",
+        "pyqtgraph!=0.13.3",
         "PyOpenGL",
         "colorcet",
         "biopython>=1.78",
